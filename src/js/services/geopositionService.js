afbc1ed5ccd94840f6af5bfcb8750b965a215572
Kirill <mister.semushin@gmail.com>
2021-06-18 21:36:37 +0300

    v.1.0.0

--- src/js/services/geopositionService.js
+++ src/js/services/geopositionService.js
@@ -3,13 +3,12 @@ import backgroundImageService from '../services/background-image-service';
 import { defaultReqWeather } from '../render/renderOneDay';
 import { setBackgroundImage } from '../components/background-image';
 
-//Геопоизиция АПИ используем сервисное апи для рендера заднего фона 
+//Геопоизиция АПИ используем сервисное Axios апи для рендера заднего фона, дефолтный фон cloudy
 navigator.geolocation.getCurrentPosition(success, defaultData);
 
 function defaultData() {
   defaultReqWeather('Lipetsk');
 }
-
 function success(position) {
   const apiKey = 'd4683b09d0c94ec0aebf0b2e043decbf';
   const coordinates = position.coords;
@@ -27,7 +26,6 @@ function success(position) {
       setBackground(location);
     });
 }
-
 function setBackground(location) {
   backgroundImageService.query = location;
   backgroundImageService
